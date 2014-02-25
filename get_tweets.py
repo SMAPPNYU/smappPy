3faf8a582ebc7d3c7861e7a965a4fc065c7583cc
dpenfoldbrown <dpenfoldbrown@gmail.com>
2014-02-24 19:31:22 -0500

    Added initial base get_tweets module

---
+++ get_tweets.py
@@ -0,0 +1,18 @@
+"""
+Functions for getting tweets from Twitter (via REST API)
+
+@auth dpb
+@date 2/24/2014
+"""
+
+def keyword_tweets():
+    """"""
+    pass
+
+def user_tweets():
+    """"""
+    pass
+
+def geo_tweets():
+    """"""
+    pass
