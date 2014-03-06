593984d93b2382c2119fe645c28ff245360bd652
dpenfoldbrown <dpenfoldbrown@gmail.com>
2014-03-05 23:16:31 -0500

    Added get_tweets functions for list and IT

--- get_tweets.py
+++ get_tweets.py
@@ -1,5 +1,5 @@
 """
-Functions for getting tweets from Twitter (via REST API)
+Functions for getting tweets from Twitter (via REST API), file, and database
 
 @auth dpb
 @date 2/24/2014
@@ -14,5 +14,35 @@ def user_tweets():
     pass
 
 def geo_tweets():
+    """"""
+    pass
+
+def tweets_from_file(tweetfile):
+    """
+    Returns a list of tweets read from given file. Tweets are dicts representing json of 
+    file contents. For a less-memory intensive version, consider the tweet_from_file_IT() 
+    function.
+    Note: will not check validity of tweet. Will simply return dict representations of
+    JSON entities in the file
+    """
+    from bson.json_util import loads
+    from json_util import ConcatJSONDecoder
+
+    with open(tweetfile) as handle:
+        tweets = loads(handle.read(), cls=ConcatJSONDecoder)
+    return tweets
+
+def tweets_from_file_IT(tweetfile):
+    """
+    Returns an iterator for tweets in given tweetfile. Tweets are considered dict
+    representations of JSON in given tweetfile
+    """
+    from bson.json_util import loads
+    
+    with open(tweetfile) as handle:
+        for line in handle:
+            yield loads(line.strip())
+
+def tweets_from_db():
     """"""
     pass
