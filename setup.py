df40ebbf13fbc3966041941aeb246e451e184ef7
Jonathan Ronen <yablee@gmail.com>
2014-04-10 12:36:24 -0400

    Change package structure to implement distutil

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='smappPy',
+      version='0.1.0',
+      description='NYU SMaPP lab utility library',
+      author='NYU SMaPP',
+      license='GPLv2',
+      author_email='smapp_programmer-group@nyu.edu',
+      url='http://smapp.nyu.edu',
+      packages=['smappPy'],
+      requires=[
+          'tweepy',
+          'bson',
+          'urllib',
+          'simplejson',
+          'matplotlib',
+      ],
+     )
