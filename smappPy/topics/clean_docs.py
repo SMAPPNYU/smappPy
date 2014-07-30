80bc2135e2ac1cda532b29c7d81951898addaf3b
dpenfoldbrown <dpenfoldbrown@gmail.com>
2014-07-29 23:23:41 -0400

    Changed call style to take list of cleaning methods

--- smappPy/topics/clean_docs.py
+++ smappPy/topics/clean_docs.py
@@ -7,8 +7,20 @@ from collections import defaultdict
 from tempfile import NamedTemporaryFile
 
 
-def clean_docs(doc_iterator, out_handle, stopwords, keep_hashtags, 
-        keep_mentions, occurrence_threshold=3):
+def clean_docs(doc_iterator, 
+               out_handle, 
+               clean_functions=[translate_shorthand, 
+                                translate_unicode, 
+                                translate_contractions,
+                                remove_RT_MT,
+                                remove_link_text,
+                                remove_punctuation,
+                                http_cleaner,
+                                remove_short_words,
+                                clean_whitespace],
+               stopwords=[], 
+               occurrence_threshold=3,
+               print_progress_every=1):
     """
     Given an iterator over documents, cleans each document and then writes to
     given out_handle. 
@@ -19,49 +31,25 @@ def clean_docs(doc_iterator, out_handle, stopwords, keep_hashtags,
       occurrence_threshold  - number of times a word must appear in the corpus
                               to not be removed from all documents.
 
-    Text cleaning via smappPy and nltk (stopwords):
-      1 - remove trailing newlines and lowercase
-      2 - translate common twitter shorthand (EN)
-      3 - translate troublesome unicode chars into ASII
-      4 - translate contractions into long form (EN)
-      5 - remove hanging RT and MT strings
-      6 - remove ALL punctuation (except mention and hastag symbols)
-      7 - normalize whitespace
-      8 - remove all stopwords
-      9 - apply 'occurrence_threshold'
+    Text cleaning via chain of functions (see smappPy.text_clean for examples)
     """
     print "Building up word counts and cleaning document text"
     word_counts = defaultdict(int)
     tmp_handle = NamedTemporaryFile()
     doc_count = 1
     for doc in doc_iterator:
-        print "Processing doc {0}".format(doc_count)
+        if doc_count % print_progress_every == 0:
+            print "Processing doc {0}".format(doc_count)
         doc_count += 1
 
         doc = doc.strip().lower()
-        # print ".. Trans shorthand"
-        doc = translate_shorthand(doc)
-        # print ".. Trans unicode"
-        doc = translate_unicode(doc)
-        # print ".. Trans contractions"
-        doc = translate_contractions(doc)
-        # print ".. Remove RT/MT"
-        doc = remove_RT_MT(doc)
-        # print ".. Remove Link text"
-        doc = remove_link_text(doc)     # MUST BEFORE PUNCTUATION
-        # print ".. Remove punctuation"
-        doc = remove_punctuation(doc)
-        # print ".. Remove hanging 'http's"
-        doc = http_cleaner(doc)
-        # print ".. Remove short words"
-        doc = remove_short_words(doc)
-        # print ".. Remove stopwords"
-        doc = clean_whitespace(doc)
+        for cf in clean_functions:
+            doc = cf(doc)
+        
         doc = " " + doc + " " 
         for w in stopwords:
             doc = doc.replace(u" {0} ".format(w.decode("utf8")), u" ")
         doc = doc.strip()
-
 
         for w in doc.split():
             word_counts[w] += 1
