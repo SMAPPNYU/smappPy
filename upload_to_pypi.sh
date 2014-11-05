ff5fad50ffebf0e7baa1c5dfff2223c8f0bbc2b6
Jonathan Ronen <yablee@gmail.com>
2014-11-05 15:36:43 -0500

    Bump version number

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+sed -r "s/(\s+)version='([0-9]\.[0-9]\.)([0-9]+)'/echo \"\1version='\2\$((\3+1))'\"/ge" setup.py > tempfile_version
+rm setup.py
+mv tempfile_version setup.py
+git commit -am "Bump version number"
 python setup.py sdist upload
