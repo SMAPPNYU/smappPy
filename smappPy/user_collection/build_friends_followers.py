c11104c5a3e253b0db81107c385aa3266e0e3c92
dpenfoldbrown <dpenfoldbrown@gmail.com>
2014-12-08 16:41:37 -0500

    Re-factored build friends followers fctly

--- smappPy/user_collection/build_friends_followers.py
+++ smappPy/user_collection/build_friends_followers.py
@@ -1,152 +1,214 @@
 """
-Script containing functions (and a runable Main function) to populate friend and
-follower User objects from a collection of seed users.
+Script containing functions to populate friend and follower User objects from
+a collection of seed users.
 
-NOTE: Followers/list and friends/list endpoints have a less rate-limited API
-endpoint than do followers/ids and friends/ids (for some reason).
+@auth dpb
+@date 12/01/2014
 """
 
-import warnings
 import argparse
-from random import random
+import warnings
+from tweepy import Cursor
 from datetime import datetime
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
+from smappPy.tweepy_pool import APIPool
 from smappPy.tweepy_error_handling import call_with_error_handling
+#TODO: Local imports to test, change to smappPy when working
+#from smappPy.user_collection.userdocs import ensure_userdoc_indexes, create_userdoc
+#from smappPy.user_collection.network_edges import create_edge_doc, ensure_edge_indexes
+from userdocs import ensure_userdoc_indexes, create_userdoc
+from network_edges import create_edge_doc, ensure_edge_indexes
 
+#TODO: def get_friend_ids_sample(api, user_id, user_doc=None, sample=0.1)
+#TODO: Gets sample proportion of friends. Takes userdoc - if None, queries
+#TODO: rest API for user, get num friends, iterates over cursor until sample
+#TODO: number is hit
 
-def populate_friends(api, user_id, seed_collection, friend_collection, sample=1.0):
+#TODO: def get_friends(api, user_id)
+#TODO: Gets fully-hydrated friend user docs via Tweepy 'friends' method
+
+def get_friends_ids(api, user_id):
     """
-    From a seed collection of User documents (any doc with an 'id' field representing
-    a valid twitter user ID), queries twitter REST API friends/list endpoint for fully
-    populated friend user objects for given user, stores them in given friend_collection.
-    
-    - seed_collection and friend_collection fully connected, authenticated mongo collection
-    objects
-    - user_id, either from or to be stored in seed_collection
-    - api fully authenticated tweepy API object or smappPy tweepyPool object
-    - sample is proportion of friends to fetch (a limiter)
-
-    - Adds user objects to friends collection, and also to User's "friend_ids" list. 
-    - Updates User's friends_updated timestamp.
-
-    Currently only does "one hop," ie gets only the seed users' friends
-    (not the friends' subsequent friends.)
-
-    TODO: Obvs make so it can just take a list of user IDs, not an entire collection of users.
-
-    TODO: Do in slices of 100 userIDs, so can either make sure have a user obj in DB or get from
-    twitter if not
+    Given a Tweepy/smappPy TweepyPool api, query twitter's rest API for friends of
+    given user_id. Returns IDs only (much faster / more per request).
+    Parameters:
+        api     - fully authenticated Tweepy api or smappPy TweepyPool api
+        user_id - twitter user id
+    Returns list of IDs or None (if API call fails)
     """
-    user_obj = seed_collection.find_one({"id": user_id})
-    if not user_obj:
-        user_obj = {}
-        user_obj["id"] = int(uid)
-        user_obj["random_number"] = random()
-        user_obj["updated_timestamp"] = None
-        user_obj["tweet_ids"] = None
-        user_obj["tweet_frequency"] = 0
-        user_obj["tweets_updated"] = None
-        user_obj["friend_ids"] = None
-        user_obj["follower_ids"] = None
-        user_obj["friends_updated"] = None
-        user_obj["followers_updated"] = None
+    user_list, ret_code = call_with_error_handling(list,
+        Cursor(api.friends_ids, user_id=user_id).items())
+
+    if ret_code != 0:
+        warnings.warn("User {0}: Friends request failed")
     
-    friends_list, return_code = call_with_error_handling(api.friends, user_id=user_obj["id"])
-    if return_code != 0:
-        warnings.warn("Friends call received error {0}\n.. Skipping user {1}".format(return_code, user_id))
-        return
-    
-    friend_ids = []
-    for friend in friends_list:
-        raw_friend = friend._json
-        friend_ids.append(raw_friend["id"])
+    # Retun user list from API or None (call_with_error_handling returns None if
+    # call fail)
+    return user_list
 
-        raw_friend["random_number"] = random()
-        raw_friend["updated_timestamp"] = datetime.now()
-        raw_friend["tweet_ids"] = None
-        raw_friend["tweet_frequency"] = 0
-        raw_friend["tweets_updated"] = None
-        raw_friend["friend_ids"] = None
-        raw_friend["follower_ids"] = None
-        raw_friend["friends_updated"] = None
-        raw_friend["followers_updated"] = None
-        raw_friend["hop"] = 1
-
-        try:
-            friend_collection.save(raw_friend)
-        except DuplicateKeyError:
-            warnings.warn("Friend {0} already in friends collection".format(raw_friend["id"]))
-
-    if user_obj["friend_ids"] == None:
-        user_obj["friend_ids"] = []
-
-    for fid in friend_ids:
-        if fid not in user_obj["friend_ids"]:
-            user_obj["friend_ids"].append(fid)
-
-    try:
-        seed_collection.save(user_obj)
-    except DuplicateKeyError:
-        warnings.warn("Seed user {0} object duplicated in DB".format(user_id))
-
-
-
-def populate_followers(api, user_id, seed_collection, follower_collection, sample=1.0):
+def get_followers_ids(api, user_id):
     """
-    See 'populate_friends'. Same, but for followers.
+    Given a Tweepy/smappPy TweepyPool api, query twitter's rest API for followers of
+    given user_id. Returns IDs only (much faster / more per request).
+    Parameters:
+        api     - fully authenticated Tweepy api or smappPy TweepyPool api
+        user_id - twitter user id
+    Returns list of IDs or None (if API call fails)
     """
-    user_obj = seed_collection.find_one({"id": user_id})
-    if not user_obj:
-        user_obj = {}
-        user_obj["id"] = int(uid)
-        user_obj["random_number"] = random()
-        user_obj["updated_timestamp"] = None
-        user_obj["tweet_ids"] = None
-        user_obj["tweet_frequency"] = 0
-        user_obj["tweets_updated"] = None
-        user_obj["friend_ids"] = None
-        user_obj["follower_ids"] = None
-        user_obj["friends_updated"] = None
-        user_obj["followers_updated"] = None
+    user_list, ret_code = call_with_error_handling(list,
+        Cursor(api.followers_ids, user_id=user_id).items())
+
+    if ret_code != 0:
+        warnings.warn("User {0}: Followers request failed")
     
-    followers_list, return_code = call_with_error_handling(api.followers, user_id=user_obj["id"])
-    if return_code != 0:
-        warnings.warn("Followers call received error {0}\n.. Skipping user {1}".format(return_code, user_id))
-        return
-    
-    follower_ids = []
-    for follower in followers_list:
-        raw_follower = friend._json
-        follower_ids.append(raw_follower["id"])
+    # Retun user list from API or None (call_with_error_handling returns None if
+    # call fail)
+    return user_list
 
-        raw_follower["random_number"] = random()
-        raw_follower["updated_timestamp"] = datetime.now()
-        raw_follower["tweet_ids"] = None
-        raw_follower["tweet_frequency"] = 0
-        raw_follower["tweets_updated"] = None
-        raw_follower["friend_ids"] = None
-        raw_follower["follower_ids"] = None
-        raw_follower["friends_updated"] = None
-        raw_follower["followers_updated"] = None
-        raw_follower["hop"] = 1
+#TODO: Add friends_sample functionality, depending on get_friend_ids_sample
+def populate_friends_from_collection(api, seed_collection, friend_collection, edge_collection=None,
+    user_sample=1.0, print_progress_every=1000):
+    """
+    Populates given 'friends_collection' with local user documents representing the friends
+    of each user in given 'seed_collection'.
+    Note: populated documents are NOT fully hydrated twitter user objects, just IDs with 
+    status fields. 
+    'friend_ids' field of seed_collection user docs will also be updated with IDs of friends
+    fetched from twitter API.
+    Parameters:
+        api     - fully authenticated Tweepy api or smappPy TweepyPool api
+        seed_collection    - fully authenticated (read/write) mongo collection
+        friend_collection  - fully authenticated (read/write) mongo collection
+        edge_collection    - [OPTIONAL] collection to store simple edge: {to: ID, from: ID}
+        user_sample        - proportion of seed users to fetch friends for
+    """
+    # Ensure indexes
+    ensure_userdoc_indexes(seed_collection)
+    ensure_userdoc_indexes(friend_collection)
+    if edge_collection:
+        ensure_edge_indexes(edge_collection)
 
-        try:
-            follower_collection.save(raw_follower)
-        except DuplicateKeyError:
-            warnings.warn("Follower {0} already in friends collection".format(raw_follower["id"]))
+    # Sample users (if necessary)
+    if user_sample < 1.0:
+        seed_count = seed_collection.count()
+        users = seed_collection.find(limit=int(seed_count * user_sample))
+    else:
+        users = seed_collection.find()
 
-    if user_obj["follower_ids"] == None:
-        user_obj["follower_ids"] = []
+    # Progress vars
+    user_count = users.count(with_limit_and_skip=True)
+    user_it = 1
 
-    for fid in follower_ids:
-        if fid not in user_obj["follower_ids"]:
-            user_obj["follower_ids"].append(fid)
+    # Iterate over users, get friends, save user and friends
+    friend_request_failed_for = []
+    for user in users:
+        if user_it % print_progress_every == 0:
+            print ".. Processing user {0} of {1}".format(user_it, user_count)
+        user_it += 1
 
-    try:
-        seed_collection.save(user_obj)
-    except DuplicateKeyError:
-        warnings.warn("Seed user {0} object duplicated in DB".format(user_id))
+        friend_ids = get_friends_ids(api, user["id"])
+        if friend_ids == None:
+            friend_request_failed_for.append(user["id"])
+            continue
+
+        # Initialize (if necessary) and set user's friend_ids list
+        if not user["friend_ids"]:
+            user["friend_ids"] = list(set(friend_ids))
+        else:
+            user["friend_ids"] = list(set(user["friend_ids"] + friend_ids))
+
+        for friend_id in friend_ids:
+            # Create and save friend doc to friend collection
+            friend_doc = create_userdoc(friend_id)
+            try:
+                friend_collection.save(friend_doc)
+            except DuplicateKeyError:
+                #warnings.warn("Friend {0} already in friends collection".format(
+                #    friend_id))
+                continue
+
+        # Optionally save "edge" document
+        if edge_collection:
+            for friend_id in friend_ids:
+                edge_doc = create_edge_doc(user["id"], friend_id)
+                try:
+                    edge_collection.save(edge_doc)
+                except DuplicateKeyError:
+                    warnings.warn("Edge {0} alread in DB, skipping".format(
+                        edge_doc))
+
+        # Update user doc's timestamps and save
+        user["updated_timestamp"] = datetime.now()
+        user["friends_updated"] = datetime.now()
+        seed_collection.save(user)
+
+
+#TODO: Add followers_sample functionality, depending on get_followers_ids_sample
+def populate_followers_from_collection(api, seed_collection, follower_collection, edge_collection=None,
+    user_sample=1.0, print_progress_every=1000):
+    """
+    See 'populate_friends_from_collection'. Exactly the same, but for followers
+    """
+    # Ensure indexes
+    ensure_userdoc_indexes(seed_collection)
+    ensure_userdoc_indexes(follower_collection)
+    if edge_collection:
+        ensure_edge_indexes(edge_collection)
+
+    # Sample users (if necessary)
+    if user_sample < 1.0:
+        seed_count = seed_collection.count()
+        users = seed_collection.find(limit=int(seed_count * user_sample))
+    else:
+        users = seed_collection.find()
+
+    # Progress vars
+    user_count = users.count(with_limit_and_skip=True)
+    user_it = 1
+
+    # Iterate over users, get followers, save user and followers
+    follower_request_failed_for = []
+    for user in users:
+        if user_it % print_progress_every == 0:
+            print ".. Processing user {0} of {1}".format(user_it, user_count)
+        user_it += 1
+
+        follower_ids = get_followers_ids(api, user["id"])
+        if follower_ids == None:
+            follower_request_failed_for.append(user["id"])
+            continue
+
+        # Initialize (if necessary) and set user's follower_ids list
+        if not user["follower_ids"]:
+            user["follower_ids"] = list(set(follower_ids))
+        else:
+            user["follower_ids"] = list(set(user["follower_ids"] + follower_ids))
+
+        for follower_id in follower_ids:
+            # Create and save follower doc to follower collection
+            follower_doc = create_userdoc(follower_id)
+            try:
+                follower_collection.save(follower_doc)
+            except DuplicateKeyError:
+                #warnings.warn("Follower {0} already in followers collection".format(
+                #   follower_id))
+                continue
+
+        # Optionally save "edge" document
+        if edge_collection:
+            for follower_id in follower_ids:
+                edge_doc = create_edge_doc(follower_id, user["id"])
+                try:
+                    edge_collection.save(edge_doc)
+                except DuplicateKeyError:
+                    warnings.warn("Edge {0} alread in DB, skipping".format(
+                        edge_doc))
+
+        # Update user doc's timestamps and save
+        user["updated_timestamp"] = datetime.now()
+        user["followers_updated"] = datetime.now()
+        seed_collection.save(user)
 
 
 if __name__ == "__main__":
@@ -167,10 +229,16 @@ if __name__ == "__main__":
         help="Collection in which to store friends [None]")
     parser.add_argument("-foc", "--followers_collection", default=None,
         help="Collection in which to store followers [None]")
+    parser.add_argument("-ec", "--edge_collection", default=None,
+        help="Collection in which to store network edges [None]")
     parser.add_argument("-o", "--oauthsfile", required=True,
-        help="Twiiter oauths file. JSON file w/ LIST of app documents")
-
+        help="Twitter oauths file. JSON file w/ LIST of app documents")
+    parser.add_argument("-ppe", "--print_progress_every", type=int,
+        default=1000, help="Print progress every Nth user [1000]")
     args = parser.parse_args()
+
+    # Set up TweepyPool API
+    api = APIPool(oauths_filename=args.oauthsfile, debug=True)
 
     # Set up DB connection
     client = MongoClient(args.server, args.port)
@@ -178,13 +246,25 @@ if __name__ == "__main__":
     if args.user and args.password:
         database.authenticate(args.user, args.password)
     seed_collection = database[args.seed_collection]
-    friend_collection = database[args.friends_collection]
-    follower_collection = database[args.followers_collection]
+    edge_collection = database[args.edge_collection] if args.edge_collection else None
 
-    # Set up TweepyPool API
-    api = APIPool(oauths_filename=args.oauthsfile, debug=True)
+    if args.friends_collection:
+        print "Populating Friends from {0}".format(seed_collection)
+        friend_collection = database[args.friends_collection]
+        populate_friends_from_collection(api, seed_collection, friend_collection, 
+            edge_collection=edge_collection, user_sample=1.0,
+            print_progress_every=args.print_progress_every)
+        print "Friends complete"
+    
+    if args.followers_collection:
+        print "Populating Followers from {0}".format(seed_collection)
+        follower_collection = database[args.followers_collection]
+        populate_followers_from_collection(api, seed_collection, follower_collection, 
+            edge_collection=edge_collection, user_sample=1.0,
+            print_progress_every=args.print_progress_every)
+        print "Followers complete"
 
-    # Iterate over all users in seed collection
+
 
 
 
