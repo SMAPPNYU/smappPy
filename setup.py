6d1b02f1c0051b617e37880575efece19f654dc5
dpenfoldbrown <dpenfoldbrown@gmail.com>
2015-03-05 14:39:06 -0500

    Revert "Bumped version number"
    This reverts commit 86f80fcad9e097b0da151290e4cb296cd9995d87.
    
    Conflicts:
    	setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='smappPy',
+      version='0.1.8',
+      description='NYU SMaPP lab utility library',
+      author='NYU SMaPP',
+      license='GPLv2',
+      author_email='smapp_programmer-group@nyu.edu',
+      url='http://smapp.nyu.edu',
+      packages=['smappPy'],
+      requires=[
+          'tweepy',
+          'bson',
+          'urllib',
+          'simplejson',
+          'matplotlib',
+      ],
+     )
