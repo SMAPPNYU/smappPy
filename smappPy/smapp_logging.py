5306fb18a6cbac07ea24346031da54553d045873
Jonathan Ronen <yablee@gmail.com>
2015-04-02 14:09:25 -0400

    Fix logger format

--- smappPy/smapp_logging.py
+++ smappPy/smapp_logging.py
@@ -4,4 +4,4 @@ Logging module. Imports stdlib logging and sets some of our favorite basicConfig
 @jonathanronen 2015/4
 """
 import logging
-logging.basicConfig(format='%(asctime)s\t%(pathname)s:%(lineno)s(%(func)s)--%(levelname)s: %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s\t%(pathname)s:%(lineno)s--%(levelname)s: %(message)s', level=logging.INFO)
