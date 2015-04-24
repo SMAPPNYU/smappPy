e3cdae839eac6dfdc1b557fc82ce70982c6b6811
Jonathan Ronen <yablee@gmail.com>
2015-04-24 19:25:25 -0400

    Also get rid of unicode control characters at end of spectrum....

--- smappPy/xml_util.py
+++ smappPy/xml_util.py
@@ -4,6 +4,6 @@ Utility functions relating to dealing with xml files.
 @jonathanronen 2015/4
 """
 
-mpa = dict.fromkeys(range(20))
+mpa = dict.fromkeys(range(0,32) + range(127,160))
 def clear_unicode_control_chars(text):
     return text.translate(mpa)
