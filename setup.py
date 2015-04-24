12f3615734ade6e4e182ca2e0d64a3a83e2f8310
Jonathan Ronen <yablee@gmail.com>
2015-04-24 19:25:33 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='smappPy',
-      version='0.1.15',
+      version='0.1.16',
       description='NYU SMaPP lab utility library',
       author='NYU SMaPP',
       license='GPLv2',
