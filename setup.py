fe9e5f3d8d0ead4b0db9d1771a004856ed200a46
dpb <dpenfoldbrown@gmail.com>
2015-05-05 19:10:59 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='smappPy',
-      version='0.1.17',
+      version='0.1.18',
       description='NYU SMaPP lab utility library',
       author='NYU SMaPP',
       license='GPLv2',
--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-RET=`sed -r "s/(\s+)version='([0-9]\.[0-9]\.)([0-9]+)'/echo \"\1version='\2\$((\3+1))'\"/ge" setup.py > tempfile_version`
-if ["$RET" -ne "0"]; then
-    echo "Parsing command number failed, exiting"
-    exit 1;
-fi
-rm setup.py
+sed -r "s/(\s+)version='([0-9]\.[0-9]\.)([0-9]+)'/echo \"\1version='\2\$((\3+1))'\"/ge" setup.py > tempfile_version
 mv tempfile_version setup.py
 git commit -am "Bump version number"
 python setup.py sdist upload
