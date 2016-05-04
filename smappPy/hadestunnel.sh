ed11a5032e1380400feb56c33f6079a3c1b5f041
yvan <yvanscher@gmail.com>
2016-05-04 14:02:54 -0400

    reorg

--- smappPy/hadestunnel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-scriptpath=$(cd "$(dirname "$0")"; pwd)
-
-#kill screen sessions that already have the name hades tunnel.
-# for session in $(screen -ls | grep -o '[0-9]*\.hades'); do screen -S "${session}" -X quit; done
-
-#create a new screen and execute our interactive bash script inside it
-screen -dmS "hadestunnel" $scriptpath/hadesexpect.sh
